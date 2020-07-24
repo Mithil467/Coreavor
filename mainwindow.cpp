7f0d0572b0abc7fd644995a8f97c5e52afea8c4f
Mithil <mithil467@gmail.com>
2020-07-25 00:36:07 +0530

    Scale images to fit
    Set minimum size for window

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,13 +7,15 @@
 #include <QMessageBox>
 #include <QTextStream>
 #include <QFontDialog>
+#include<QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    this->resize(580, 460);
+    this->resize(560, 420);
+
     connect(ui->actionOpen, &QAction::triggered, this, &MainWindow::open);
 }
 
@@ -24,18 +26,51 @@ MainWindow::~MainWindow()
 
 void MainWindow::open()
 {
-    QString fileName = QFileDialog::getOpenFileName(this, "Open the file");
+    QString fileName = QFileDialog::getOpenFileName(this, "Open image", QDir::homePath()+"/Pictures");
     QFile file(fileName);
     currentFile = fileName;
-    QGraphicsScene *scene;
-    QPixmap image;
 
-    image.load(fileName);
+    // Reset scale
+    ui->graphicsView->resetMatrix();
 
-    scene = new QGraphicsScene(this);
-    scene->addPixmap(image);
-    scene->setSceneRect(image.rect());
+    // Image is a Pixmap
+    this->image.load(fileName);
+
+    this->scene = new QGraphicsScene(this);
+
+    this->scene->addPixmap(image);
+    this->scene->setSceneRect(image.rect());
     ui->graphicsView->setScene(scene);
-    ui->graphicsView->scale(1.5,1.5);
+
+
+    int y = this->size().height() - image.height();
+    int x = this->size().width() - image.width();
+    double scaleFactor = 1.0;
+    double xsf = this->size().width()/(double)image.width();
+    double ysf = this->size().height()/(double)image.height();
+    // There are 4 cases here -
+    // 1. (+x,+y) Image fits well in the slot -> Do nothing
+    // 2. (-x, +y) Image extends vertically
+    // 3. (+x, -y) Image extends horizontally
+    // 4. (-x, -y) Image extends both directions -> This has 2 cases
+
+    if(x<0 && y<0)
+    {
+        if(x < y)
+            scaleFactor = xsf;
+        else
+            scaleFactor = ysf;
+    }
+    if(x<0 && y>=0)
+        scaleFactor = xsf;
+   if(x>=0 && y<0)
+       scaleFactor = ysf;
+   scaleFactor -= 0.015;
+
+
+        ui->graphicsView->scale(scaleFactor, scaleFactor);
+
+
+
     setWindowTitle(fileName);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <QMainWindow>
+#include "ui_mainwindow.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,6 +18,8 @@ public:
 private:
     Ui::MainWindow *ui;
     QString currentFile;
+    QGraphicsScene *scene;
+    QPixmap image;
 
     private slots:
         void open();
